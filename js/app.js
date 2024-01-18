5875c26cd2663a240a072fbe800791522dfaff51
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-01-18 17:32:20 -0400

    feat: add function dropdown

--- js/app.js
+++ js/app.js
@@ -1,55 +1,69 @@
 // <=========== SCROLL HEADER ===========>
 function scrollHeader() {
-  const nav = document.getElementById('header');
-  if (this.scrollY >= 50) nav.classList.add('active-header');
-  else nav.classList.remove('active-header');
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 50) nav.classList.add("active-header");
+  else nav.classList.remove("active-header");
 }
 // evento adicionado ao iniciar o scroll...
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
 // <=========== DROPDOWN MENU ===========>
 
-const dropdownMenu1 = document.getElementById('dropdown-item-1');
-const dropdownMenu2 = document.getElementById('dropdown-item-2');
-const dropdownMenu3 = document.getElementById('dropdown-item-3');
+const dropdownMenu1 = document.getElementById("dropdown-item-1");
+const dropdownMenu2 = document.getElementById("dropdown-item-2");
+const dropdownMenu3 = document.getElementById("dropdown-item-3");
 
-const menuItem1 = document.getElementById('menu-item-1');
-const menuItem2 = document.getElementById('menu-item-2');
-const menuItem3 = document.getElementById('menu-item-3');
+const menuItem1 = document.getElementById("menu-item-1");
+const menuItem2 = document.getElementById("menu-item-2");
+const menuItem3 = document.getElementById("menu-item-3");
 
 function showDropdownMenu1(event) {
   event.preventDefault();
-  dropdownMenu1.classList.toggle('active-dropdown-menu');
-  dropdownMenu2.classList.remove('active-dropdown-menu');
-  dropdownMenu3.classList.remove('active-dropdown-menu');
+  dropdownMenu1.classList.toggle("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
 }
 
 function showDropdownMenu2(event) {
   event.preventDefault();
-  dropdownMenu2.classList.toggle('active-dropdown-menu');
-  dropdownMenu1.classList.remove('active-dropdown-menu');
-  dropdownMenu3.classList.remove('active-dropdown-menu');
+  dropdownMenu2.classList.toggle("active-dropdown-menu");
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
 }
 
 function showDropdownMenu3(event) {
   event.preventDefault();
-  dropdownMenu3.classList.toggle('active-dropdown-menu');
-  dropdownMenu1.classList.remove('active-dropdown-menu');
-  dropdownMenu2.classList.remove('active-dropdown-menu');
+  dropdownMenu3.classList.toggle("active-dropdown-menu");
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
 }
 
-menuItem1.addEventListener('click', showDropdownMenu1);
-menuItem2.addEventListener('click', showDropdownMenu2);
-menuItem3.addEventListener('click', showDropdownMenu3)
+menuItem1.addEventListener("click", showDropdownMenu1);
+menuItem2.addEventListener("click", showDropdownMenu2);
+menuItem3.addEventListener("click", showDropdownMenu3);
 
 function closeDropdownMenu() {
-  dropdownMenu1.classList.remove('active-dropdown-menu');
-  dropdownMenu2.classList.remove('active-dropdown-menu');
-  dropdownMenu3.classList.remove('active-dropdown-menu');
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
 }
 
-dropdownMenu1.addEventListener('mouseleave', closeDropdownMenu);
-dropdownMenu2.addEventListener('mouseleave', closeDropdownMenu);
-dropdownMenu3.addEventListener('mouseleave', closeDropdownMenu);
+dropdownMenu1.addEventListener("mouseleave", closeDropdownMenu);
+dropdownMenu2.addEventListener("mouseleave", closeDropdownMenu);
+dropdownMenu3.addEventListener("mouseleave", closeDropdownMenu);
 
-// #Projeto: Nubank | 15 - Codificação: Menu pt.2
+// Menu Mobile
+
+const showMenu = (toglleId, navId) => {
+  const toggle = document.getElementById(toglleId);
+  const nav = document.getElementById(navId);
+
+  if(toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('active-menu-mobile');
+      toggle.classList.toggle('active-bx');
+    })
+  }
+};
+
+showMenu('bx', 'menu-mobile');
